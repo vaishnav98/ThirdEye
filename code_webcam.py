2259a83276b73cf1bcd0f44419dc01043b7e1807
Aaron Xavier <32948803+aaronxavier@users.noreply.github.com>
2018-11-14 18:19:15 +0530

    Create code_webcam.py
    Code to stream webcam feed on your computer.

---
+++ code_webcam.py
@@ -0,0 +1,29 @@
+import cv2
+import sys
+from time import sleep
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
